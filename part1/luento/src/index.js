1d71d4a1fbe2efd319bed4e49c3505210296760e
saarabot <saara.pekkala@gmail.com>
2019-03-21 12:01:12 +0200

    osa 1c

--- part1/luento/src/index.js
+++ part1/luento/src/index.js
@@ -1,29 +1,63 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 
-const Hello = (props) => {
+const Hello = ({ name , age}) => {
+    const bornYear = () => new Date().getFullYear() - age;
     return (
         <div>
-            <p>Hello {props.name}, you are {props.age} years old</p>
+            <p>Hello {name}, you are {age} years old</p>
+            <p>So you were probably born {bornYear()}</p>
         </div>
     )
 }
 
-const App = () => {
-    console.log('hello from component');
-    const now = new Date();
+const Display = ({ counter }) => <div>{counter}</div>;
+
+const Button = ({ handleClick, text }) => (
+    <button onClick={handleClick}>
+        {text}
+    </button>
+);
+
+const App = (props) => {
+    const [ counter, setCounter ] = useState(0);
+
+    /*setTimeout(
+        () => setCounter(counter + 1),
+        1000
+    );*/
+    
+    /*
+    const increaseByOne = () => {
+        setCounter(counter + 1);
+    }
+
+    const setToZero = () => {
+        setCounter(0);
+    }*/
+
+    const setToValue = (value) => () => setCounter(value);
+
     const a = 10;
     const b = 20;
     const name="Pertti";
+
     return (
         <div>
             <h1>Greetings</h1>
             <Hello name="Olavi" age={a+b}/>
             <Hello name={name} age={b}/>
-        </div>
+            <Display counter={counter} />
+            <Button text="plus" handleClick={setToValue(counter+1)}/>
+            <Button text="zero" handleClick={setToValue(0)}/>
+            <Button text="minus" handleClick={setToValue(counter-1)}/>        </div>
     )
 }
 
+
 ReactDOM.render(<App />, document.getElementById('root'));
+
+
+
 
 

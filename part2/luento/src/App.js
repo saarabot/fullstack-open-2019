2fbe4f779e2f92fb8cdf7c2e97ea88c9ad40171f
saarabot <saara.pekkala@gmail.com>
2019-03-24 10:09:19 +0200

    luento osa 2c

--- part2/luento/src/App.js
+++ part2/luento/src/App.js
@@ -1,10 +1,20 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import Note from './components/Note';
 
 const App = (props) => {
-    const [ notes, setNotes ] = useState(props.notes);
+    const [ notes, setNotes ] = useState([]);
     const [newNote, setNewNote] =useState('uusi muistiinpano...');
     const [showAll, setShowAll] = useState(true);
+
+    useEffect(() => {
+        console.log('effect');
+        axios.get('http://localhost:3001/notes').then(response => {
+            console.log('promise fullfilled');
+            setNotes(response.data);
+        })
+    }, []);
+    console.log('render', notes.length, 'notes');
 
     const notesToShow = showAll ? notes : notes.filter(note => note.important);
 

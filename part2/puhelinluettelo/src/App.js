f0ed93f4ae8ef8290cd083b28fd312d3ca9b923c
saarabot <saara.pekkala@gmail.com>
2019-03-24 10:22:35 +0200

    puhelinluettelo step6

--- part2/puhelinluettelo/src/App.js
+++ part2/puhelinluettelo/src/App.js
@@ -1,18 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Filter from './components/Filter';
 import NewContact from './components/NewContact';
 import Contacts from './components/Contacts';
+import axios from 'axios';
 
 const App = () => {
-    const [persons, setPersons] = useState([
-        { name: 'Arto Hellas', number: '040-123456' },
-        { name: 'Martti Tienari', number: '040-123456' },
-        { name: 'Arto Järvinen', number: '040-123456' },
-        { name: 'Lea Kutvonen', number: '040-123456' }
-      ]);
+
+    const [ persons, setPersons ] = useState([]);
     const [ newName, setNewName ] = useState('');
     const [ newNumber, setNewNumber ] = useState('');
     const [ filter, setFilter ] = useState('');
+
+    useEffect(() => {
+        axios.get('http://localhost:3001/persons').then(response => {
+            setPersons(response.data);
+        })
+    }, []);
 
     const addNewContact = (event) => {
         event.preventDefault();
